2a42e5359d2a4c5e25251d247472e8bf9ac65a0f
= <127305775+prettyRag@users.noreply.github.com>
2024-03-23 21:05:14 +0300

    change

--- todoo/asgi.py
+++ todoo/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for todoList project.
+ASGI config for todoo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-#hhhh
+
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todoList.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todoo.settings")
 
 application = get_asgi_application()
