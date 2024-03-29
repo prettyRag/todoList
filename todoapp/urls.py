799c2399c8a6942d681c04ed858cd3ac53d7a326
= <127305775+prettyRag@users.noreply.github.com>
2024-03-29 21:53:07 +0300

    done

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,7 +4,6 @@ urlpatterns = [
     path('', views.home, name='home-page'),
     path('register/', views.register, name='register'),
     path('login/', views.loginpage, name='login'),
-    path('logout/', views.LogoutView, name='logout'),
     path('delete-task/<str:name>/', views.DeleteTask, name='delete'),
     path('update/<str:name>/', views.Update, name='update'),
 ]
